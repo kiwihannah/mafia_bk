a332619eac26364bdf1849435ff07a7a4740c913
Hannah <gkssk8827@gmail.com>
2022-03-09 00:28:20 +0900

    중간 푸쉬

--- controllers/game.js
+++ controllers/game.js
@@ -2,17 +2,12 @@ const gameService = require('../services/game');
 
 module.exports = {
   create: {
-    ready: async (_, res) => {
-      const { nickname } = req.body;
-      const user = await gameService.create.user({ nickname });
-      return res.status(201).json({ user });
+    readyGroup: async (req, res) => {
+      const { roomId, userId } = req.params;
+      await gameService.create.user({ roomId, userId });
+      return res.status(201);
     },
   },
 
-  update: {
-    ready: async (_, res) => {
-      const user = await gameService.create.user({ nickname });
-      return res.status(201).json({ user });
-    },
-  },
+  
 };
