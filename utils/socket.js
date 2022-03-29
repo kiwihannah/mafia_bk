a380bc44975c067cb1011e0633d834e730389318
Hannah <gkssk8827@gmail.com>
2022-03-29 16:11:16 +0900

    오류 해결

--- utils/socket.js
+++ utils/socket.js
@@ -57,13 +57,13 @@ module.exports = (server) => {
     // 레디 카운트 1
     socket.on('readyCnt', async (data) => {
       const { roomId, userId, socketId } = data;
-      console.log('@@@@ 받은 데이터--->' ,roomId, userId, socketId );
+      console.log('@@@@111 받은 데이터--->' ,roomId, userId, socketId );
       const readyUser = await GameGroup.findOne({ where: { roomId, userId } });
       await readyUser.update({ isReady: 'Y' });
 
       const users = await GameGroup.findAll({ where: { roomId, isReady: 'Y' } });
       const readyCnt = users.length;
-      console.log('@@@@@보낼 데이터--->', readyCnt)
+      console.log('@@@@@111 보낼 데이터--->', readyCnt)
       socket.in(roomId).emit('readyCnt', { readyCnt });
       socket.to(socketId).emit('readyCnt', { readyCnt });
     });
@@ -71,13 +71,13 @@ module.exports = (server) => {
     // 레디 카운트 2
     socket.on('cancelReady', async (data) => {
       const { roomId, userId, socketId } = data;
-      console.log('@@@@ 받은 데이터--->' ,roomId, userId, socketId );
+      console.log('@@@@222 받은 데이터--->' ,roomId, userId, socketId );
       const readyUser = await GameGroup.findOne({ where: { roomId, userId } });
       await readyUser.update({ isReady: 'N' });
 
-      const users = await GameGroup.findAll({ where: { roomId, isReady: 'Y' } });
+      const users = await GameGroup.findAll({ where: { roomId, isReady: 'N' } });
       const readyCnt = users.length;
-      console.log('@@@@@보낼 데이터--->', readyCnt)
+      console.log('@@@@@222 보낼 데이터--->', readyCnt)
       socket.in(roomId).emit('readyCnt', { readyCnt });
       socket.to(socketId).emit('readyCnt', { readyCnt });
     });
