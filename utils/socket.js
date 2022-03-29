75e775eee4bf7fc4b1058a8bb80cf144ece42eef
Hannah <gkssk8827@gmail.com>
2022-03-29 16:05:38 +0900

    콘솔 추가

--- utils/socket.js
+++ utils/socket.js
@@ -57,24 +57,28 @@ module.exports = (server) => {
     // 레디 카운트 1
     socket.on('readyCnt', async (data) => {
       const { roomId, userId, socketId } = data;
+      console.log('@@@@ 받은 데이터--->' ,roomId, userId, socketId );
       const readyUser = await GameGroup.findOne({ where: { roomId, userId } });
       await readyUser.update({ isReady: 'Y' });
 
       const users = await GameGroup.findAll({ where: { roomId, isReady: 'Y' } });
       const readyCnt = users.length;
-      socket.on(roomId).emit('readyCnt', { readyCnt });
+      console.log('@@@@@보낼 데이터--->', readyCnt)
+      socket.in(roomId).emit('readyCnt', { readyCnt });
       socket.to(socketId).emit('readyCnt', { readyCnt });
     });
 
     // 레디 카운트 2
     socket.on('cancelReady', async (data) => {
       const { roomId, userId, socketId } = data;
+      console.log('@@@@ 받은 데이터--->' ,roomId, userId, socketId );
       const readyUser = await GameGroup.findOne({ where: { roomId, userId } });
       await readyUser.update({ isReady: 'N' });
 
       const users = await GameGroup.findAll({ where: { roomId, isReady: 'Y' } });
       const readyCnt = users.length;
-      socket.on(roomId).emit('readyCnt', { readyCnt });
+      console.log('@@@@@보낼 데이터--->', readyCnt)
+      socket.in(roomId).emit('readyCnt', { readyCnt });
       socket.to(socketId).emit('readyCnt', { readyCnt });
     });
 
